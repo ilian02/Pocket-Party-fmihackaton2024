54c4aec1dbe8607162740d7cdb20cb3054ed4656
Bozhidar <bobkatakio@gmail.com>
2024-03-16 12:18:27 +0200

    creating canvas

--- hackaton2024/Cosmic Co-pilot/index.js
+++ hackaton2024/Cosmic Co-pilot/index.js
@@ -1,17 +1,69 @@
-//board
-let board;
-let boardWidth = 1960;
-let boardHeight = 1080;
-let contex;
+const SHIP_HEIGHT = 50;
+const SHIP_WIDTH = 60;
 
-window.onload = function() {
-    board=document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
 
-    context = board.getContext("2d");
-    
+let canvas;
+let canvasWidth = 1920;
+let canvasHeight = 1080;
+let context;
+
+let game = {};
+
+let backgroundImage = new Image();
+backgroundImage.src = "./assets/im2.png"
+
+// Define canvas and context
+game.canvas = document.getElementById('canvas');
+game.ctx = game.canvas.getContext('2d');
+
+// Define background color
+game.backgroundColor = '#FF0000';
+
+// Defines a function to handle the game loop
+game.update = function() {
+// Draw canvas background
+    game.ctx.fillStyle = game.backgroundColor;
+    game.ctx.fillRect(20, 20, game.canvas.width, game.canvas.height);
+    game.ctx.drawImage(backgroundImage, 20, 20, canvasWidth, canvasHeight);
+
 }
+
+// Defines a function to handle key events
+game.keydown = function(e) {
+
+}      
+
+// Defines a function to start the game loop
+game.init = function() {
+
+// Set the game loop
+    game.interval = setInterval(game.update, 1000 / 60);
+}
+
+// Defines a function to stop the game loop
+game.stop = function() {
+clearInterval(game.interval);
+}
+
+// Defines a function to restart the game
+game.restart = function() {
+    game.stop();
+    game.init();
+}
+
+// Start the game on window load
+window.onload = game.init;
+
+class Ship {
+    constructor(collBox, imageURL) {
+        this.collBox = collBox;  //CollisionBox(100, 100, settings.SHIP_WIDTH, settings.SHIP_HEIGHT);
+        this.image = new Image();
+        this.image.src = imageURL;
+    }
+}
+
+
+
 
 class CollisionBox {
     constructor(x, y, width, height) {
