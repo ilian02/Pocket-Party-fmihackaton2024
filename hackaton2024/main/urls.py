725deb5c9a52b928ae73528f6c1d0cef30acb792
ilian02 <36775333+ilian02@users.noreply.github.com>
2024-03-16 05:20:00 +0200

    added lobby and join lobby views

--- hackaton2024/main/urls.py
+++ hackaton2024/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import lobbies, join_lobby
 
 urlpatterns = [
-
+    path('', lobbies),
+    path('join_lobby', join_lobby)
 ] 
--- hackaton2024/main/views.py
+++ hackaton2024/main/views.py
@@ -1,3 +1,12 @@
+from django.views import View
+from django.http import HttpResponse
 from django.shortcuts import render
+from .lobby import Lobby
 
-# Create your views here.
+def lobbies(request):
+    lobbies = [Lobby('Lobby A', 1), Lobby('Lobby B', 2), Lobby('Lobby C', 3)]
+
+    return render(request, 'lobbies.html', {'lobbies': lobbies})
+
+def join_lobby(request):
+    return render(request, 'controlers.html')
